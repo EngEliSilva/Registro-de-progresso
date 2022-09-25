667d17c2bfc1f1f123b623c95240ea9e463d2fbe
EngEliSilva <elisilva.luiz@hotmail.com>
2022-09-25 14:48:58 -0300

    Adicionando certificado: 'Conhecendo Python'

--- Teste.py
+++ Teste.py
@@ -1,1 +1,13 @@
-print("Salve!")
+from typing import Concatenate
+
+
+print("Preencha as informações abaixo:")
+nome = input("Nome: ")
+sobrenome = input("Sobrenome: ")
+print("Data de nascimento:")
+dia = input("dia: ")
+mes = input("mês: ")
+ano = input("ano: ")
+nascimento = (dia, mes, ano)
+print(dia, mes, ano)
+str
